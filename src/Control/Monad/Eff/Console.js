17c178908454033b37189512698e618138d8279f
Gary Burgess <gary.burgess@gmail.com>
2016-01-17 17:04:40 +0000

    Updates for PureScript 0.8

--- src/Control/Monad/Eff/Console.js
+++ src/Control/Monad/Eff/Console.js
@@ -1,4 +1,3 @@
-/* global exports, console */
 "use strict";
 
 // module Control.Monad.Eff.Console
--- src/Control/Monad/Eff/Console.purs
+++ src/Control/Monad/Eff/Console.purs
@@ -1,19 +1,27 @@
--- | Console-related functions and effect type.
 module Control.Monad.Eff.Console where
 
-import Prelude
+import Control.Monad.Eff (Eff)
 
-import Control.Monad.Eff
+import Data.Show (class Show, show)
+import Data.Unit (Unit)
 
--- | The `CONSOLE` effect represents those computations which write to the console.
+-- | The `CONSOLE` effect represents those computations which write to the
+-- | console.
 foreign import data CONSOLE :: !
 
 -- | Write a message to the console.
-foreign import log :: forall eff. String -> Eff (console :: CONSOLE | eff) Unit
+foreign import log
+  :: forall eff
+   . String
+  -> Eff (console :: CONSOLE | eff) Unit
 
 -- | Write an error to the console.
-foreign import error :: forall eff. String -> Eff (console :: CONSOLE | eff) Unit
+foreign import error
+  :: forall eff
+   . String
+  -> Eff (console :: CONSOLE | eff) Unit
 
--- | Write a value to the console, using its `Show` instance to produce a `String`.
-print :: forall a eff. (Show a) => a -> Eff (console :: CONSOLE | eff) Unit
-print = log <<< show
+-- | Write a value to the console, using its `Show` instance to produce a
+-- | `String`.
+print :: forall a eff. Show a => a -> Eff (console :: CONSOLE | eff) Unit
+print a = log (show a)
